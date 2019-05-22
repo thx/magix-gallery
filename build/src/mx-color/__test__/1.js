f061af98962afeabd33e1d28a9cfeb68bde2574f
yujia <yujia.yjq@alibaba-inc.com>
2019-05-22 14:45:14 +0800

    publish 1.3.12

--- build/src/mx-color/__test__/1.js
+++ build/src/mx-color/__test__/1.js
@@ -1,81 +1,1 @@
-/*
-    generate by magix-combine@3.11.28: https://github.com/thx/magix-combine
-    author: kooboy_li@163.com
-    loader: cmd_es
- */
-define("mx-color/__test__/1",["magix","__test__/example","$","../picker","mx-copy/index","__test__/hl"],(require,exports,module)=>{
-/*Magix,Base,$*/
-require("../picker");
-require("mx-copy/index");
-require("__test__/hl");
-var Magix = require("magix");
-var Base = require("__test__/example");
-var $ = require("$");
-module.exports = Base.extend({
-    tmpl: function ($$, $viewId, $$ref, $e, $n, $eu, $i, $eq) { if (!$$ref)
-    $$ref = $$; if (!$n) {
-    var $em_1 = { '&': 'amp', '<': 'lt', '>': 'gt', '"': '#34', '\'': '#39', '`': '#96' }, $er_1 = /[&<>"'`]/g, $ef_1 = function (m) { return "&" + $em_1[m] + ";"; };
-    $n = function (v) { return '' + (v == null ? '' : v); };
-    $e = function (v) { return $n(v).replace($er_1, $ef_1); };
-} if (!$eu) {
-    var $um_1 = { '!': '%21', '\'': '%27', '(': '%28', ')': '%29', '*': '%2A' }, $uf_1 = function (m) { return $um_1[m]; }, $uq_1 = /[!')(*]/g;
-    $eu = function (v) { return encodeURIComponent($n(v)).replace($uq_1, $uf_1); };
-} if (!$eq) {
-    var $qr_1 = /[\\'"]/g;
-    $eq = function (v) { return $n(v).replace($qr_1, '\\$&'); };
-} ; var $g = '', $_temp, $p = '', color = $$.color, viewId = $$.viewId, text1 = $$.text1, text2 = $$.text2; var $expr, $art, $line; try {
-    $p += '<div mxa="_zs_galleryar:_" class="_zs_gallery___test___layout_-example"><div mxa="_zs_galleryar:a" class="_zs_gallery___test___layout_-eg-content"><div mxa="_zs_galleryar:b" class="mb20"><span mxa="_zs_galleryar:c" class="color-9">当前选中颜色：';
-    $line = 4;
-    $art = '=color';
-    ;
-    $p += ($expr = '<%=color%>', $e(color)) + '</span></div><div class="w200" mx-change="' + $viewId + 'changeColor()" mx-view="mx-color/picker?color=';
-    $line = 7;
-    $art = '=color';
-    ;
-    $p += ($expr = '<%!$eu(color)%>', $eu(color)) + '"></div></div><div mxa="_zs_galleryar:d" class="_zs_gallery___test___layout_-eg-desc"><div mxs="_zs_galleryar:_" class="_zs_gallery___test___layout_-eg-title">HTML Code</div><div class="_zs_gallery___test___layout_-desc-oper" mx-success="' + $viewId + 'done({id:1})" mx-view="mx-copy/index?copyNode=';
-    $line = 12;
-    $art = '=viewId';
-    ;
-    $p += ($expr = '<%!$eu(viewId)%>', $eu(viewId)) + '_text_1"><span mxa="_zs_galleryar:e" class="_zs_gallery___test___layout_-desc-tip">';
-    $line = 14;
-    $art = '!text1';
-    ;
-    $p += ($expr = '<%!text1%>', $n(text1)) + '</span><i mxs="_zs_galleryar:a" class="mc-iconfont _zs_gallery___test___layout_-desc-icon">&#xe610;</i></div><pre mx-view="__test__/hl" id="';
-    $line = 17;
-    $art = '=viewId';
-    ;
-    $p += ($expr = '<%=viewId%>', $e(viewId)) + '_text_1">\n&lt;mx-color.picker class="w200"\n    color="&#123;&#123;=color&#125;&#125;"\n    mx-change="changeColor()"/&gt;\n        </pre></div><div mxa="_zs_galleryar:f" class="_zs_gallery___test___layout_-eg-desc"><div mxs="_zs_galleryar:b" class="_zs_gallery___test___layout_-eg-title">JS Code</div><div class="_zs_gallery___test___layout_-desc-oper" mx-success="' + $viewId + 'done({id:2})" mx-view="mx-copy/index?copyNode=';
-    $line = 25;
-    $art = '=viewId';
-    ;
-    $p += ($expr = '<%!$eu(viewId)%>', $eu(viewId)) + '_text_2"><span mxa="_zs_galleryar:g" class="_zs_gallery___test___layout_-desc-tip">';
-    $line = 27;
-    $art = '!text2';
-    ;
-    $p += ($expr = '<%!text2%>', $n(text2)) + '</span><i mxs="_zs_galleryar:a" class="mc-iconfont _zs_gallery___test___layout_-desc-icon">&#xe610;</i></div><pre mx-view="__test__/hl" id="';
-    $line = 30;
-    $art = '=viewId';
-    ;
-    $p += ($expr = '<%=viewId%>', $e(viewId)) + '_text_2">\nlet Magix = require(\'magix\');\nlet $ = require(\'$\');\n\nmodule.exports = Magix.View.extend(&#123;\n    tmpl: \'@index.html\',\n    render() &#123;\n        this.updater.digest(&#123;\n            color: \'#9a3c3c\'\n        &#125;);\n    &#125;,\n    \'changeColor&lt;change&gt;\'(e)&#123;\n        this.updater.digest(&#123;\n            color: e.color\n        &#125;);\n    &#125;\n&#125;);\n        </pre></div></div>';
-}
-catch (ex) {
-    var msg = 'render view error:' + (ex.message || ex);
-    if ($art)
-        msg += '\r\n\tsrc art:{{' + $art + '}}\r\n\tat line:' + $line;
-    msg += '\r\n\t' + ($art ? 'translate to:' : 'expr:');
-    msg += $expr + '\r\n\tat file:mx-color/__test__/1.html';
-    throw msg;
-} return $p; },
-    render: function () {
-        this.updater.digest({
-            color: '#9a3c3c'
-        });
-    },
-    'changeColor<change>': function (e) {
-        this.updater.digest({
-            color: e.color
-        });
-    }
-});
-
-});
+define("mx-color/__test__/1",["magix","__test__/example","$","../picker","mx-copy/index","__test__/hl"],(e,l,a)=>{e("../picker"),e("mx-copy/index"),e("__test__/hl");e("magix");var r=e("__test__/example");e("$");a.exports=r.extend({tmpl:function(e,l,a,r,s,_,n,i){if(a||(a=e),!s){var c={"&":"amp","<":"lt",">":"gt",'"':"#34","'":"#39","`":"#96"},t=/[&<>"'`]/g,o=function(e){return"&"+c[e]+";"};s=function(e){return""+(null==e?"":e)},r=function(e){return s(e).replace(t,o)}}if(!_){var d={"!":"%21","'":"%27","(":"%28",")":"%29","*":"%2A"},x=function(e){return d[e]},g=/[!')(*]/g;_=function(e){return encodeURIComponent(s(e)).replace(g,x)}}if(!i){var m=/[\\'"]/g;i=function(e){return s(e).replace(m,"\\$&")}}var p="",v=e.color,y=e.viewId,u=e.text1,z=e.text2;return p+='<div mxa="_zs_galleryar:_" class="_zs_galleryg"><div mxa="_zs_galleryar:a" class="_zs_galleryj"><div mxa="_zs_galleryar:b" class="mb20"><span mxa="_zs_galleryar:c" class="color-9">当前选中颜色：'+r(v)+'</span></div><div class="w200" mx-change="'+l+'changeColor()" mx-view="mx-color/picker?color='+_(v)+'"></div></div><div mxa="_zs_galleryar:d" class="_zs_galleryk"><div mxs="_zs_galleryar:_" class="_zs_galleryi">HTML Code</div><div class="_zs_galleryl" mx-success="'+l+'done({id:1})" mx-view="mx-copy/index?copyNode='+_(y)+'_text_1"><span mxa="_zs_galleryar:e" class="_zs_galleryn">'+s(u)+'</span><i mxs="_zs_galleryar:a" class="mc-iconfont _zs_gallerym">&#xe610;</i></div><pre mx-view="__test__/hl" id="'+r(y)+'_text_1">\n&lt;mx-color.picker class="w200"\n    color="&#123;&#123;=color&#125;&#125;"\n    mx-change="changeColor()"/&gt;\n        </pre></div><div mxa="_zs_galleryar:f" class="_zs_galleryk"><div mxs="_zs_galleryar:b" class="_zs_galleryi">JS Code</div><div class="_zs_galleryl" mx-success="'+l+'done({id:2})" mx-view="mx-copy/index?copyNode='+_(y)+'_text_2"><span mxa="_zs_galleryar:g" class="_zs_galleryn">'+s(z)+'</span><i mxs="_zs_galleryar:a" class="mc-iconfont _zs_gallerym">&#xe610;</i></div><pre mx-view="__test__/hl" id="'+r(y)+"_text_2\">\nlet Magix = require('magix');\nlet $ = require('$');\n\nmodule.exports = Magix.View.extend(&#123;\n    tmpl: '@index.html',\n    render() &#123;\n        this.updater.digest(&#123;\n            color: '#9a3c3c'\n        &#125;);\n    &#125;,\n    'changeColor&lt;change&gt;'(e)&#123;\n        this.updater.digest(&#123;\n            color: e.color\n        &#125;);\n    &#125;\n&#125;);\n        </pre></div></div>"},render:function(){this.updater.digest({color:"#9a3c3c"})},"changeColor<change>":function(e){this.updater.digest({color:e.color})}})});
--- build/src/mx-popover/__test__/1.js
+++ build/src/mx-popover/__test__/1.js
@@ -1,78 +1,1 @@
-/*
-    generate by magix-combine@3.11.28: https://github.com/thx/magix-combine
-    author: kooboy_li@163.com
-    loader: cmd_es
- */
-define("mx-popover/__test__/1",["magix","__test__/example","$","../index","mx-copy/index","__test__/hl"],(require,exports,module)=>{
-/*Magix,Base,$*/
-require("../index");
-require("mx-copy/index");
-require("__test__/hl");
-var Magix = require("magix");
-var Base = require("__test__/example");
-var $ = require("$");
-module.exports = Base.extend({
-    tmpl: function ($$, $viewId, $$ref, $e, $n, $eu, $i, $eq) { if (!$$ref)
-    $$ref = $$; if (!$n) {
-    var $em_1 = { '&': 'amp', '<': 'lt', '>': 'gt', '"': '#34', '\'': '#39', '`': '#96' }, $er_1 = /[&<>"'`]/g, $ef_1 = function (m) { return "&" + $em_1[m] + ";"; };
-    $n = function (v) { return '' + (v == null ? '' : v); };
-    $e = function (v) { return $n(v).replace($er_1, $ef_1); };
-} if (!$eu) {
-    var $um_1 = { '!': '%21', '\'': '%27', '(': '%28', ')': '%29', '*': '%2A' }, $uf_1 = function (m) { return $um_1[m]; }, $uq_1 = /[!')(*]/g;
-    $eu = function (v) { return encodeURIComponent($n(v)).replace($uq_1, $uf_1); };
-} if (!$eq) {
-    var $qr_1 = /[\\'"]/g;
-    $eq = function (v) { return $n(v).replace($qr_1, '\\$&'); };
-} ; var $g = '', $_temp, $p = '', viewId = $$.viewId, text1 = $$.text1, text2 = $$.text2, text3 = $$.text3; var $expr, $art, $line; try {
-    $p += '<div mxa="_zs_galleryc/:_" class="_zs_gallery___test___layout_-example"><div mxs="_zs_galleryc/:_" class="_zs_gallery___test___layout_-eg-content"><div class="mb20"><span class="color-9">说明：</span>提示浮层内文案默认左对齐</div><span class="btn mr20" mx-view="mx-popover/index?content=%E9%BB%98%E8%AE%A4%E4%B8%8B%E4%B8%AD%E9%97%B4%E5%AF%B9%E9%BD%90">默认下中间对齐</span><span class="btn mr20" mx-view="mx-popover/index?content=%E4%B8%8B%E5%B7%A6%E5%AF%B9%E9%BD%90&placement=bottom&align=left">下左对齐</span><span class="btn" mx-view="mx-popover/index?content=%E4%B8%8B%E5%8F%B3%E5%AF%B9%E9%BD%90&placement=bottom&align=right">下右对齐</span></div><div mxa="_zs_galleryc/:a" class="_zs_gallery___test___layout_-eg-desc"><div mxs="_zs_galleryc/:a" class="_zs_gallery___test___layout_-eg-title">下中间对齐</div><div class="_zs_gallery___test___layout_-desc-oper" mx-success="' + $viewId + 'done({id:1})" mx-view="mx-copy/index?copyNode=';
-    $line = 20;
-    $art = '=viewId';
-    ;
-    $p += ($expr = '<%!$eu(viewId)%>', $eu(viewId)) + '_text_1"><span mxa="_zs_galleryc/:b" class="_zs_gallery___test___layout_-desc-tip">';
-    $line = 22;
-    $art = '!text1';
-    ;
-    $p += ($expr = '<%!text1%>', $n(text1)) + '</span><i mxs="_zs_galleryc/:b" class="mc-iconfont _zs_gallery___test___layout_-desc-icon">&#xe610;</i></div><pre mx-view="__test__/hl" id="';
-    $line = 25;
-    $art = '=viewId';
-    ;
-    $p += ($expr = '<%=viewId%>', $e(viewId)) + '_text_1">\n&lt;mx-popover class="btn"\n    content="默认下中间对齐"&gt;默认下中间对齐&lt;/mx-popover&gt;</pre></div><div mxa="_zs_galleryc/:c" class="_zs_gallery___test___layout_-eg-desc"><div mxs="_zs_galleryc/:c" class="_zs_gallery___test___layout_-eg-title">下左对齐</div><div class="_zs_gallery___test___layout_-desc-oper" mx-success="' + $viewId + 'done({id:2})" mx-view="mx-copy/index?copyNode=';
-    $line = 31;
-    $art = '=viewId';
-    ;
-    $p += ($expr = '<%!$eu(viewId)%>', $eu(viewId)) + '_text_2"><span mxa="_zs_galleryc/:d" class="_zs_gallery___test___layout_-desc-tip">';
-    $line = 33;
-    $art = '!text2';
-    ;
-    $p += ($expr = '<%!text2%>', $n(text2)) + '</span><i mxs="_zs_galleryc/:b" class="mc-iconfont _zs_gallery___test___layout_-desc-icon">&#xe610;</i></div><pre mx-view="__test__/hl" id="';
-    $line = 36;
-    $art = '=viewId';
-    ;
-    $p += ($expr = '<%=viewId%>', $e(viewId)) + '_text_2">\n&lt;mx-popover class="btn"\n    content="下左对齐"\n    placement="bottom"\n    align="left"&gt;下左对齐&lt;/mx-popover&gt;</pre></div><div mxa="_zs_galleryc/:e" class="_zs_gallery___test___layout_-eg-desc"><div mxs="_zs_galleryc/:d" class="_zs_gallery___test___layout_-eg-title">下右对齐</div><div class="_zs_gallery___test___layout_-desc-oper" mx-success="' + $viewId + 'done({id:3})" mx-view="mx-copy/index?copyNode=';
-    $line = 44;
-    $art = '=viewId';
-    ;
-    $p += ($expr = '<%!$eu(viewId)%>', $eu(viewId)) + '_text_3"><span mxa="_zs_galleryc/:f" class="_zs_gallery___test___layout_-desc-tip">';
-    $line = 46;
-    $art = '!text3';
-    ;
-    $p += ($expr = '<%!text3%>', $n(text3)) + '</span><i mxs="_zs_galleryc/:b" class="mc-iconfont _zs_gallery___test___layout_-desc-icon">&#xe610;</i></div><pre mx-view="__test__/hl" id="';
-    $line = 49;
-    $art = '=viewId';
-    ;
-    $p += ($expr = '<%=viewId%>', $e(viewId)) + '_text_3">\n&lt;mx-popover class="btn"\n    content="下右对齐"\n    placement="bottom"\n    align="right"&gt;下右对齐&lt;/mx-popover&gt;</pre></div></div>';
-}
-catch (ex) {
-    var msg = 'render view error:' + (ex.message || ex);
-    if ($art)
-        msg += '\r\n\tsrc art:{{' + $art + '}}\r\n\tat line:' + $line;
-    msg += '\r\n\t' + ($art ? 'translate to:' : 'expr:');
-    msg += $expr + '\r\n\tat file:mx-popover/__test__/1.html';
-    throw msg;
-} return $p; },
-    render: function () {
-        this.updater.digest();
-    }
-});
-
-});
+define("mx-popover/__test__/1",["magix","__test__/example","$","../index","mx-copy/index","__test__/hl"],(e,s,l)=>{e("../index"),e("mx-copy/index"),e("__test__/hl");e("magix");var _=e("__test__/example");e("$");l.exports=_.extend({tmpl:function(e,s,l,_,t,n,a,c){if(l||(l=e),!t){var i={"&":"amp","<":"lt",">":"gt",'"':"#34","'":"#39","`":"#96"},r=/[&<>"'`]/g,x=function(e){return"&"+i[e]+";"};t=function(e){return""+(null==e?"":e)},_=function(e){return t(e).replace(r,x)}}if(!n){var o={"!":"%21","'":"%27","(":"%28",")":"%29","*":"%2A"},m=function(e){return o[e]},p=/[!')(*]/g;n=function(e){return encodeURIComponent(t(e)).replace(p,m)}}if(!c){var d=/[\\'"]/g;c=function(e){return t(e).replace(d,"\\$&")}}var g="",v=e.viewId,y=e.text1,z=e.text2,u=e.text3;return g+='<div mxa="_zs_galleryc/:_" class="_zs_galleryg"><div mxs="_zs_galleryc/:_" class="_zs_galleryj"><div class="mb20"><span class="color-9">说明：</span>提示浮层内文案默认左对齐</div><span class="btn mr20" mx-view="mx-popover/index?content=%E9%BB%98%E8%AE%A4%E4%B8%8B%E4%B8%AD%E9%97%B4%E5%AF%B9%E9%BD%90">默认下中间对齐</span><span class="btn mr20" mx-view="mx-popover/index?content=%E4%B8%8B%E5%B7%A6%E5%AF%B9%E9%BD%90&placement=bottom&align=left">下左对齐</span><span class="btn" mx-view="mx-popover/index?content=%E4%B8%8B%E5%8F%B3%E5%AF%B9%E9%BD%90&placement=bottom&align=right">下右对齐</span></div><div mxa="_zs_galleryc/:a" class="_zs_galleryk"><div mxs="_zs_galleryc/:a" class="_zs_galleryi">下中间对齐</div><div class="_zs_galleryl" mx-success="'+s+'done({id:1})" mx-view="mx-copy/index?copyNode='+n(v)+'_text_1"><span mxa="_zs_galleryc/:b" class="_zs_galleryn">'+t(y)+'</span><i mxs="_zs_galleryc/:b" class="mc-iconfont _zs_gallerym">&#xe610;</i></div><pre mx-view="__test__/hl" id="'+_(v)+'_text_1">\n&lt;mx-popover class="btn"\n    content="默认下中间对齐"&gt;默认下中间对齐&lt;/mx-popover&gt;</pre></div><div mxa="_zs_galleryc/:c" class="_zs_galleryk"><div mxs="_zs_galleryc/:c" class="_zs_galleryi">下左对齐</div><div class="_zs_galleryl" mx-success="'+s+'done({id:2})" mx-view="mx-copy/index?copyNode='+n(v)+'_text_2"><span mxa="_zs_galleryc/:d" class="_zs_galleryn">'+t(z)+'</span><i mxs="_zs_galleryc/:b" class="mc-iconfont _zs_gallerym">&#xe610;</i></div><pre mx-view="__test__/hl" id="'+_(v)+'_text_2">\n&lt;mx-popover class="btn"\n    content="下左对齐"\n    placement="bottom"\n    align="left"&gt;下左对齐&lt;/mx-popover&gt;</pre></div><div mxa="_zs_galleryc/:e" class="_zs_galleryk"><div mxs="_zs_galleryc/:d" class="_zs_galleryi">下右对齐</div><div class="_zs_galleryl" mx-success="'+s+'done({id:3})" mx-view="mx-copy/index?copyNode='+n(v)+'_text_3"><span mxa="_zs_galleryc/:f" class="_zs_galleryn">'+t(u)+'</span><i mxs="_zs_galleryc/:b" class="mc-iconfont _zs_gallerym">&#xe610;</i></div><pre mx-view="__test__/hl" id="'+_(v)+'_text_3">\n&lt;mx-popover class="btn"\n    content="下右对齐"\n    placement="bottom"\n    align="right"&gt;下右对齐&lt;/mx-popover&gt;</pre></div></div>'},render:function(){this.updater.digest()}})});
