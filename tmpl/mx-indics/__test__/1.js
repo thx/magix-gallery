64dd037ac894e983a0eb78a44c89969c88249cb1
yujia <yujia.yjq@alibaba-inc.com>
2020-11-24 17:31:32 +0800

    mx-indics 分组支持批量操作

--- tmpl/mx-indics/__test__/1.js
+++ tmpl/mx-indics/__test__/1.js
@@ -6,47 +6,47 @@ module.exports = Base.extend({
     tmpl: '@1.html',
     render() {
         let fields = [{
-            text: '消耗', 
-            value: 1, 
+            text: '消耗',
+            value: 1,
             pValue: 1,
             tip: '消耗提示文案'
         }, {
-            text: '展现量', 
-            value: 2, 
+            text: '展现量',
+            value: 2,
             pValue: 1
         }, {
-            text: '点击量', 
-            value: 3, 
+            text: '点击量',
+            value: 3,
             pValue: 1
         }, {
-            text: '点击率', 
-            value: 4, 
+            text: '点击率',
+            value: 4,
             pValue: 1
         }, {
-            text: '平均点击单价', 
-            value: 5, 
+            text: '平均点击单价',
+            value: 5,
             pValue: 1,
             tip: '平均点击单价提示文案'
         }, {
-            text: '点击转化率', 
-            value: 6, 
+            text: '点击转化率',
+            value: 6,
             pValue: 2
         }, {
-            text: '成交笔数', 
-            value: 7, 
+            text: '成交笔数',
+            value: 7,
             pValue: 2
         }, {
-            text: '淘积木页面平均停留时长', 
+            text: '淘积木页面平均停留时长',
             value: 8,
-            tip: '淘积木页面平均停留时长提示文案', 
+            tip: '淘积木页面平均停留时长提示文案',
             pValue: 2
         }, {
-            text: '投资回报率', 
-            value: 9, 
+            text: '投资回报率',
+            value: 9,
             pValue: 2
         }, {
-            text: '访问页面数', 
-            value: 10, 
+            text: '访问页面数',
+            value: 10,
             pValue: 2
         }]
 
@@ -62,17 +62,17 @@ module.exports = Base.extend({
         fields.forEach(item => {
             map[item.value] = item.text;
         })
-        
+
         this.updater.digest({
             parents,
             fields,
             custom: false, //当前是为默认数据
-            customs: [2,3,4,5],
-            defaults: [2,3],
+            customs: [2, 3, 4, 5],
+            defaults: [2, 3],
             map
         });
     },
-    'change<change>'(e){
+    'change<change>'(e) {
         // e.custom：true or false，是否为自定义指标
         // e.customs：用户自定义的指标，当前用的的配置
         // e.defaults：默认指标，不变
--- tmpl/mx-indics/dialog.ts
+++ tmpl/mx-indics/dialog.ts
@@ -5,77 +5,72 @@ Magix.applyStyle('@index.less');
 export default View.extend({
     tmpl: '@dialog.html',
     init(e) {
-        let selected = e.selected || [],
-            fields = e.fields || [],
+        let fields = e.fields || [],
             parents = e.parents || [],
-            selectedItems = [];
+            selected = e.selected || [],
+            selectedItems = [],
+            lineNumber = e.lineNumber; // 每行多少个指标
 
         selected.forEach(value => {
-            for (var i = 0; i < fields.length; i++) {
+            for (let i = 0; i < fields.length; i++) {
                 let field = fields[i];
-                if (field.value == value) {
+                if (field.value + '' === value + '') {
+                    // 按照指标的配置顺序
                     field.checked = true;
-                    selectedItems.push({
-                        value: field.value,
-                        text: field.text
-                    })
+                    selectedItems.push(field);
                     break;
                 }
             }
         })
 
-        let lineNumber = e.lineNumber;
-
-        let groups = [], hasParent;
+        let groups = [];
         if (parents.length > 0) {
             // 有分组
-            groups = parents.map(p => {
+            parents.forEach(p => {
                 let fs = [];
                 fields.forEach(f => {
                     if (f.pValue == p.value) {
                         fs.push(f);
                     }
                 })
-                return {
+                groups.push({
                     text: p.text,
                     fields: fs
-                }
+                })
             })
-            hasParent = true;
         } else {
             let num = Math.ceil(fields.length / lineNumber);
-            for (var i = 0; i < num; i++) {
-                let group = {
+            for (let i = 0; i < num; i++) {
+                groups.push({
                     fields: fields.slice(i * lineNumber, (i + 1) * lineNumber)
-                }
-                groups.push(group);
+                });
             }
-            hasParent = false;
         }
         this.updater.set({
             width: Math.floor(100 / lineNumber) + '%',
-            hasParent,
-            sortable: e.sortable,
-            fields,
+            hasParent: (parents.length > 0),
             groups,
+            fields,
             selectedItems,
+            sortable: e.sortable,
             max: e.max,
             min: e.min,
             tip: e.tip
         })
         this.viewOptions = e;
     },
+
     render() {
-        this.updater.digest({});
+        this.syncParents();
     },
+
     'toggle<change>': function (e) {
         let that = this;
         let checked = e.target.checked;
         let { value, text } = e.params;
-        let updater = that.updater;
 
-        let { fields, selectedItems, sortable } = updater.get();
-        for (var i = 0; i < fields.length; i++) {
+        let { fields, selectedItems, sortable } = that.updater.get();
+        for (let i = 0; i < fields.length; i++) {
             if (fields[i].value == value) {
                 fields[i].checked = checked;
                 break;
@@ -83,6 +78,7 @@ export default View.extend({
         }
 
         if (checked) {
+            // 选择
             if (sortable) {
                 // 可排序的时候在最后添加
                 selectedItems.push({
@@ -96,7 +92,8 @@ export default View.extend({
                 })
             }
         } else {
-            for (var i = 0; i < selectedItems.length; i++) {
+            // 移除
+            for (let i = 0; i < selectedItems.length; i++) {
                 if (selectedItems[i].value == value) {
                     selectedItems.splice(i, 1);
                     break;
@@ -104,16 +101,20 @@ export default View.extend({
             }
         }
 
-        updater.digest({
+        that.updater.set({
             fields,
             selectedItems
         });
+        that.syncParents();
     },
+
+    /**
+     * 重新排序
+     */
     'drag<dragfinish>'(e) {
-        // 重排序之后的
         let selectedItems = [];
         let drags = document.querySelectorAll('#' + this.id + ' .@index.less:drag');
-        for (var i = 0, len = drags.length; i < len; i++) {
+        for (let i = 0, len = drags.length; i < len; i++) {
             let attrs = drags[i].attributes;
             selectedItems.push({
                 value: attrs['data-value'].value,
@@ -124,6 +125,7 @@ export default View.extend({
             selectedItems
         });
     },
+
     /**
      * 恢复默认设置
      */
@@ -131,7 +133,7 @@ export default View.extend({
         let that = this;
         // 默认指标        
         let defaults = that.viewOptions.map[1].list;
-        let fields = that.updater.get('fields');
+        let { fields } = that.updater.get();
         let selectedItems = [];
         fields.forEach(field => {
             field.checked = (defaults.indexOf(field.value + '') > -1);
@@ -139,37 +141,108 @@ export default View.extend({
                 selectedItems.push(field);
             }
         })
-        that.updater.digest({
+        that.updater.set({
             fields,
             selectedItems
         });
+        that.syncParents();
     },
 
+    /**
+     * 清空
+     */
     'clear<click>'() {
         let that = this;
-        let fields = that.updater.get('fields');
+        let { fields } = that.updater.get();
         fields.forEach(field => {
             field.checked = false;
         })
 
-        that.updater.digest({
+        that.updater.set({
             fields,
             selectedItems: []
         });
+        that.syncParents();
+    },
+
+    'toggleParent<change>'(e) {
+        let checked = e.target.checked;
+        let { groupIndex } = e.params;
+        let { groups, selectedItems, max } = this.updater.get();
+
+        if (checked) {
+            // 选中
+            groups[groupIndex].fields.forEach(field => {
+                if (!field.checked && (max == 0 || (max > 0 && selectedItems.length < max))) {
+                    field.checked = true;
+                    selectedItems.push(field);
+                }
+            })
+        } else {
+            // 删除
+            groups[groupIndex].fields.forEach(field => {
+                field.checked = false;
+                for (let i = 0; i < selectedItems.length; i++) {
+                    if (selectedItems[i].value == field.value) {
+                        selectedItems.splice(i, 1);
+                        break;
+                    }
+                }
+            })
+        }
+        this.updater.set({
+            groups,
+            selectedItems
+        })
+        this.syncParents();
+    },
+
+    /**
+     * 计算组状态
+     */
+    syncParents() {
+        let { groups } = this.updater.get();
+        groups.forEach(g => {
+            let len = g.fields.length,
+                count = 0;
+            g.fields.forEach(f => {
+                if (f.checked) {
+                    count++;
+                }
+            })
+            if (count > 0) {
+                if (count == len) {
+                    // 全选
+                    g.indeterminate = false;
+                    g.checked = true;
+                } else {
+                    // 部分选中
+                    g.indeterminate = true;
+                    g.checked = false;
+                }
+            } else {
+                // 全不选
+                g.indeterminate = false;
+                g.checked = false;
+            }
+        })
+        this.updater.digest({
+            groups
+        })
     },
 
     check() {
-        event.preventDefault();
-        let { selectedItems, min } = this.updater.get();
+        let { selectedItems, min, max } = this.updater.get();
         let selected = selectedItems.map(item => {
             return item.value;
-        })
-
+        });
+        
         return new Promise((resolve) => {
             // 此处返回promise，防止有接口提交校验等
+            let len = selected.length;
             resolve({
-                ok: (selected.length >= min),
-                msg: `请至少选择 ${min} 个指标`,
+                ok: ((len >= min) && (max == 0 || (max > 0 && len <= max))),
+                msg: `请至少选择&nbsp;${min}&nbsp;个指标${(max > 0) ? ('，至多选择&nbsp;' + max + '&nbsp;个指标') : ''}`,
                 data: {
                     selected
                 }
