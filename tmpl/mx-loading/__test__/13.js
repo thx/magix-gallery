a36ee6fcd0d1c9718c29fdc7ab49d3f4f8bf3694
yujia <yujia.yjq@alibaba-inc.com>
2021-01-25 18:56:07 +0800

    test svg loading

--- tmpl/mx-loading/__test__/13.js
+++ tmpl/mx-loading/__test__/13.js
@@ -1,10 +1,21 @@
 let Magix = require('magix');
 let Base = require('__test__/example');
 let $ = require('$');
+Magix.applyStyle('@13.less')
 
 module.exports = Base.extend({
     tmpl: '@13.html',
     render() {
-        this.updater.digest();
+
+        // 半径，计算周长
+        let width = 36, border = 4;
+        let r = (width - border) / 2;
+        circle1 = Math.PI * r * 2;
+
+        this.updater.digest({
+            width,
+            border,
+            circle1
+        });
     }
 });
