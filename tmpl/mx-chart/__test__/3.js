9f4051fbc426a451a533ac273d3f68c7cb92adb4
ly195417 <ly195417@alibaba-inc.com>
2021-07-09 18:40:12 +0800

    auto commit by mm-cli

--- tmpl/mx-chart/__test__/3.js
+++ tmpl/mx-chart/__test__/3.js
@@ -6,16 +6,30 @@ module.exports = Base.extend({
     tmpl: '@3.html',
     render() {
         let chartData = [
-            {"time":"2017-03-21","pv":10,"uv":11,"click":12,"ppc":45},
-            {"time":"2017-03-22","pv":20,"uv":32,"click":14,"ppc":34},
-            {"time":"2017-03-23","pv":30,"uv":15,"click":12,"ppc":65},
-            {"time":"2017-03-24","pv":15,"uv":23,"click":21,"ppc":23},
-            {"time":"2017-03-25","pv":28,"uv":15,"click":15,"ppc":12}
+            {"time":"2020-03-21","pv":10,"uv":11,"click":12,"ppc":45},
+            {"time":"2020-03-22","pv":20,"uv":32,"click":14,"ppc":34},
+            {"time":"2020-03-23","pv":30,"uv":15,"click":12,"ppc":65},
+            {"time":"2020-03-24","pv":15,"uv":23,"click":21,"ppc":23},
+            {"time":"2020-03-25","pv":28,"uv":15,"click":15,"ppc":12}
         ];
-        let chartId = 1;
+        var options = {
+            coord: {
+                type: "rect",
+                xAxis: {
+                    field: 'time'
+                }
+            },
+            graphs: [
+                {
+                    type: "line",
+                    field: ["uv", "pv" ]
+                }
+            ],
+            legend:{}
+        };
         this.updater.digest({
             chartData,
-            chartId
+            options
         });
     }
 });
