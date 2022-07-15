198e30ad148b06730be85ff3b067e158aa190ec5
yujia <yujia.yjq@alibaba-inc.com>
2022-07-15 15:04:22 +0800

    update log

--- tmpl/__test__/update.js
+++ tmpl/__test__/update.js
@@ -12,6 +12,8 @@ module.exports = Magix.View.extend({
                 subs: [{
                     title: 'mx-tabs：支持溢出缩略显示，<a href="#!/tabs/index?highlightId=mx_12_demo24" class="color-brand">点击查看</a>'
                 }, {
+                    title: 'mx-tabs.edit：可编辑tab根据B类规范调整，支持溢出缩略显示，<a href="#!/tabs/edit" class="color-brand">点击查看</a>'
+                }, {
                     title: 'mx-effects.icon：支持圆形，方形，圆点打标，支持打标定位，<a href="#!/effects/icon" class="color-brand">点击查看</a>'
                 }, {
                     title: 'mx-breadcrumb：根据B类规范调整，支持下拉列表选项，<a href="#!/breadcrumb/index" class="color-brand">点击查看</a>'
