7d06d6a2d33eefb9ec2aaefa6c48666855ee9b18
yujia <yujia.yjq@alibaba-inc.com>
2022-08-11 19:50:14 +0800

    mx-input反白模式

--- tmpl/mx-util/monitor.js
+++ tmpl/mx-util/monitor.js
@@ -11,7 +11,7 @@ let Watcher = (e) => {
         } else {
             let view = info['@{view}'];
             if (e.type == 'resize' || !view['@{inside}'](e.target)) {
-                console.error('trigger')
+                // console.error('trigger')
                 view['@{hide}']();
             }
         }
